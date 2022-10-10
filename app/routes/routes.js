9f32e54b3daa2fac6dd7087557047e6c238e783d
Yasitha Bogamuwa <yasithab@users.noreply.github.com>
2022-10-10 23:57:14 +0400

    Adding email validation (#5)

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const Model = require('../models/model');
 const router = express.Router();
 const bearerTokenTimeout = process.env.BEARER_TOKEN_TIMEOUT || '30s';
+const { body, validationResult } = require('express-validator');
 
 // Get all Method
 router.get('/get', async (req, res) => {
@@ -35,11 +36,17 @@ router.get('/get/:id', async (req, res) => {
 })
 
 // Post Method
-router.post('/post', verifyToken, async (req, res) => {
+router.post('/post', verifyToken, body('email').isEmail(), async (req, res) => {
     jwt.verify(req.token, 'secretkey', async (err, authData) => {
         if (err) {
             res.status(403).json({message: 'Unauthorized'})
         } else {
+            // Finds the validation errors in this request
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({message: 'You must provide a valid email address'});
+            }
+
             const records = new Model({
                 name: req.body.name,
                 email: req.body.email,
@@ -61,11 +68,17 @@ router.post('/post', verifyToken, async (req, res) => {
 })
 
 // Update by ID Method
-router.patch('/update/:id', verifyToken, async (req, res) => {
+router.patch('/update/:id', verifyToken, body('email').isEmail(), async (req, res) => {
     jwt.verify(req.token, 'secretkey', async (err, authData) => {
         if (err) {
             res.status(403).json({message: 'Unauthorized'})
         } else {
+            // Finds the validation errors in this request
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({message: 'You must provide a valid email address'});
+            }
+
             try {
                 const id = req.params.id;
                 const updatedData = req.body;
